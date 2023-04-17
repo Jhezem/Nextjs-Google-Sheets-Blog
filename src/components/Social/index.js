e5bccdda1266413f1eb4d1b7af0a719a872b605d
Jhezem <mmartinhez08@gmail.com>
2023-04-16 18:34:24 -0600

    add social component

---
+++ src/components/Social/index.js
@@ -0,0 +1,17 @@
+export default function Index() {
+  return (
+    <div className="container mt-5">
+      <div className="min-h-[30vh] text-center">
+        <h3 className="text-4xl mb-5">
+          Seguime en mi canal de YouTube, me ayudarías mucho :D
+        </h3>
+        <a
+          className="text-2xl bg-indigo-600 hover:bg-indigo-700	text-white px-6 py-3 rounded-full"
+          href="https://www.youtube.com/jairopy?sub_confirmation=1"
+        >
+        <span>Subscribite</span>
+        </a>
+      </div>
+    </div>
+  );
+}
